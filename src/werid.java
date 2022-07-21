0c1c3f0056e2ccb2f9da16aca106d69a8e43549d
joice <joydingforever@gmail.com>
2022-07-20 17:27:21 -0700

    Initial commit

---
+++ src/werid.java
@@ -0,0 +1,15 @@
+class werid {
+    static String sayGood() {
+        return "T";
+    }
+    static void why(){
+        System.out.println("Did you know that ily stands for I love you?");
+    }
+    public static void main(String[] args) {
+        // Add crete methods here.
+        sayGood();
+        System.out.println(sayGood());
+        // Create method that returns void or nothing
+        why();
+    }
+}
